60783676fef7473348fea3659843eace4a6694d1
StudentLVI3 <dlya.uchby20@mail.ru>
2023-05-24 16:07:00 +0700

    QA_Lab3_Lianka
    QA_Lab3_Lianka

---
+++ LR3_TPOAS/LR3_TPOAS/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LR_6._1_С_sharp__STP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Конструктор:");
+            Point P = new Point(0, 0, 0); // Точка
+            P_A Pa = new P_A(0, 0, 0, 0); // Отрезок
+            Pa.Display();
+            Console.WriteLine("Инициализация:");
+            P.Init(1, 2, 3);
+            Pa.Init(1, 2, 3, 4);
+            float A = Pa.GetA();
+            Pa.Display();
+            A = 5;
+            Console.WriteLine("Замена:");
+            Pa.PutA(A);
+            Pa.Display();
+            A = Pa.OP();
+            float oP = P.OP();
+            Console.WriteLine("Расстояние от точки до начала координат: " + oP);
+            Console.WriteLine("Расстояние от отрезка до начала координат: " + A);
+
+            Console.Read(); // Ожидание ввода перед завершением работы
+        }
+    }
+}
